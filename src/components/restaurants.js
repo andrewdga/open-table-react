d2bd0adb58d52a3ae969f836184b49f3738d9971
andrewdga <andrew.deitrick@generalassemb.ly>
2023-09-09 10:38:57 -0600

    add restaurant component

---
+++ src/components/restaurants.js
@@ -0,0 +1,14 @@
+export default function Restaurants() {
+  return (
+    <div className="bg-gray-100">
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+        <div className="max-w-4xl mx-auto text-center">
+          <h2 className="text-3xl font-extrabold text-blue-900">Our Restaurants</h2>
+          <p className="mt-4 text-lg text-gray-500">
+            Nullam risus blandit ac aliquam justo ipsum. Quam mauris volNullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus arcuutpat massa dictumst amet. Sapien tortor
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Nav from '../components/nav'
+import Restaurants from '../components/restaurants'
 
 export default function Home() {
   return (
     <div className="">
       <Nav />
+      <Restaurants />
     </div>
   )
 }
