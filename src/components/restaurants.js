3fd98c99624df3c74462bd7c9e6f3eea1c14a9e3
andrewdga <andrew.deitrick@generalassemb.ly>
2023-09-09 10:52:54 -0600

    show restaurant names

--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -7,7 +7,6 @@ export default function Restaurants() {
     { name: 'The Noodle Shop', location: 'New York, NY' },
     { name: 'The Blue Corn', location: 'Austin, TX' }
   ]
-
   return (
     <div className="bg-gray-100">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
