2d626328c9c03b75a64f81f2a0f5d542349449f8
andrewdga <andrew.deitrick@generalassemb.ly>
2023-09-09 10:25:46 -0600

    add navigation component
    add navigation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Nav from '../components/nav'
 
 export default function Home() {
   return (
     <div className="">
-      <h1 className="text-lg text-indigo-900">Hello world</h1> 
+      <Nav />
     </div>
   )
 }
