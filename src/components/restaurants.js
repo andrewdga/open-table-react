7c7ee03d1202b079707a57a7d03c4e89557f1b5c
andrewdga <andrew.deitrick@generalassemb.ly>
2023-09-09 12:56:31 -0600

    showing reviews on the page

--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -14,32 +14,6 @@ export default function Restaurants() {
     fetchRestaurants()
   }, []);
 
-  const xrestaurants = [
-    {
-      name: 'The Golden Apple',
-      location: 'San Francisco, CA',
-      image: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
-      reviewers: [
-        { name: 'Cecilia Chavez', imageUrl: 'https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80' },
-        { name: 'Emily Selman', imageUrl: 'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80' },
-        { name: 'Kristin Watson', imageUrl: 'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80' },
-        { name: 'Celeste Burton', imageUrl: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80' },
-      ]
-    },
-    {
-      name: 'Dumpling House',
-      location: 'Portland, OR',
-      image: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
-      reviewers: [
-        { name: 'Cecilia Chavez', imageUrl: 'https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80' },
-        { name: 'Emily Selman', imageUrl: 'https://images.unsplash.com/photo-1509783236416-c9ad59bae472?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80' },
-        { name: 'Kristin Watson', imageUrl: 'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80' }
-      ]
-    },
-    { name: 'The Salty Pig', location: 'Boston, MA', image: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg', reviewers: [] },
-    { name: 'The Noodle Shop', location: 'New York, NY', image: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg', reviewers: [] },
-    { name: 'The Blue Corn', location: 'Austin, TX', image: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg', reviewers: [] },
-  ]
   return (
     <div className="bg-gray-100">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
--- src/components/restaurantsList.js
+++ src/components/restaurantsList.js
@@ -1,43 +1,52 @@
 import { ChatBubbleLeftIcon, CheckCircleIcon } from '@heroicons/react/24/outline'
 
 export default function RestaurantsList(props) {
+  function showReview(event) {
+    console.log('clicked', event.target)
+  }
+
   return (
     <ul role="list" className="divide-y divide-gray-100">
       {props.restaurants.map((restaurant) => (
-        <li
-          key={restaurant.name}
-          className="flex flex-wrap items-center justify-between gap-x-6 gap-y-4 py-5 sm:flex-nowrap"
-        >
-          <div>
-            <p className="text-sm font-semibold leading-6 text-gray-900">
-              <a href={restaurant.name} className="hover:underline">
+        <>
+          <li
+            key={restaurant.name}
+            className="flex flex-wrap items-center justify-between gap-x-6 gap-y-4 py-5 sm:flex-nowrap"
+          >
+            <div>
+              <p className="hover:underline text-sm font-semibold leading-6 text-gray-900" onClick={showReview}>
                 {restaurant.name}
-              </a>
-            </p>
-          </div>
-          <dl className="flex w-full flex-none justify-between gap-x-8 sm:w-auto">
-            <div className="flex -space-x-0.5">
-              <dt className="sr-only">Commenters</dt>
-              {restaurant.reviewers.map((reviewer) => (
-                <dd key={reviewer.name}>
-                  <img
-                    className="h-6 w-6 rounded-full bg-gray-50 ring-2 ring-white"
-                    src={reviewer.imageUrl}
-                    alt={reviewer.name}
-                  />
-                </dd>
-              ))}
+              </p>
             </div>
-            <div className="flex w-16 gap-x-2.5">
-              <dt>
-                <span className="sr-only">Total reviews</span>
-                <ChatBubbleLeftIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
-              </dt>
-              <dd className="text-sm leading-6 text-gray-900">{restaurant.reviewers.length}</dd>
-            </div>
-          </dl>
-        </li>
-      ))}
-    </ul>
+            <dl className="flex w-full flex-none justify-between gap-x-8 sm:w-auto">
+              <div className="flex -space-x-0.5">
+                <dt className="sr-only">Commenters</dt>
+                {restaurant.reviewers.map((reviewer) => (
+                  <dd key={reviewer.name}>
+                    <img
+                      className="h-6 w-6 rounded-full bg-gray-50 ring-2 ring-white"
+                      src={reviewer.imageUrl}
+                      alt={reviewer.name}
+                    />
+                  </dd>
+                ))}
+              </div>
+              <div className="flex w-16 gap-x-2.5">
+                <dt>
+                  <span className="sr-only">Total reviews</span>
+                  <ChatBubbleLeftIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
+                </dt>
+                <dd className="text-sm leading-6 text-gray-900">{restaurant.reviewers.length}</dd>
+              </div>
+            </dl>
+          </li>
+          <ul>
+            {restaurant.reviewers.map((reviewer) => (
+              <li key={reviewer.name}>{reviewer.review}</li>
+            ))}
+          </ul>
+        </>
+        ))}
+      </ul>
   )
 }
